af26ca81208883d1df3c0a415bce8ebb076c287d
David Votrubec <david.votrubec@gmail.com>
2016-09-02 10:53:10 +0200

    Replace jpg with custom gif

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,5 +1,9 @@
 console.log('Hi, I am service worker');
 
 self.addEventListener('fetch', function(event){
-    console.log('fetching', event.request);
+    console.log('fetching 2', event.request);
+    
+    if (event.request.url.indexOf('.jpg') != -1) {
+        event.respondWith(fetch('imgs/dr-evil.gif'));
+    }    
 })
