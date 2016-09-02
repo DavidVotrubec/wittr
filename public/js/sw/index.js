784b913c721a75979216d254b14d2b8eab489a05
David Votrubec <david.votrubec@gmail.com>
2016-09-02 11:48:56 +0200

    Custom 404 handling

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -5,5 +5,15 @@ self.addEventListener('fetch', function(event){
     
     if (event.request.url.endsWith('.jpg')) {
         event.respondWith(fetch('imgs/dr-evil.gif'));
-    }    
-})
+        return;
+    }
+    
+    event.respondWith(fetch(event.request).then(response => {
+        if (response.status === 404) {
+            return new Response("Whoa brother, this page was lost just like your memory.");
+        }
+        else {
+            return response;
+        }
+    }));    
+});
