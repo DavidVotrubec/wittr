f9894412ac58a051e7468629d9398b1dc1389665
David Votrubec <david.votrubec@gmail.com>
2016-09-02 10:56:00 +0200

    Using .endsWith()

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -3,7 +3,7 @@ console.log('Hi, I am service worker');
 self.addEventListener('fetch', function(event){
     console.log('fetching 2', event.request);
     
-    if (event.request.url.indexOf('.jpg') != -1) {
+    if (event.request.url.endsWith('.jpg')) {
         event.respondWith(fetch('imgs/dr-evil.gif'));
     }    
 })
